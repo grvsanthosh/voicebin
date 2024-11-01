2957e29288d52e1c12a5ea213fb24a1d2df4a393
Kumar <esnthos@amazon.com>
2024-11-02 01:40:12 +0530

    third commit

--- client/src/components/sidebar/SideBarData.jsx
+++ client/src/components/sidebar/SideBarData.jsx
@@ -9,72 +9,72 @@ export const SideBarData = [
         title: "World",
         path: "/blogfeeds",
         icon: <FaEarthAsia />,        
-        cName: 'nav-text',
-        cNav: 'current-nav',
-        role:["User", "Admin"]
+        cName: "nav-text",
+        cNav: "current-nav",
+        role:["User","Admin"]
     },
     {
         title: "Post",
         path: "/createblogs",
         icon: <FaDove />,        
-        cName: 'nav-text',
-        cNav: 'current-nav',
+        cName: "nav-text",
+        cNav: "current-nav",
         role:["User"]
     },
     {
         title: "My Post",
-        path: "/myblogs" ,
+        path: "/myblogs",
         icon: <FaRegEnvelope />,        
-        cName: 'nav-text',
-        cNav: 'current-nav',
+        cName: "nav-text",
+        cNav: "current-nav",
         role:["User"]
     },
     {
         title: "Search",
         path: "/searchblogs",
         icon: <FaSearchengin />,        
-        cName: 'nav-text',
-        cNav: 'current-nav',
+        cName: "nav-text",
+        cNav: "current-nav",
         role:["User"]
     },
     {
         title: "Settings",
         path: "/profilesettings",
         icon: <MDBIcon fas icon="cog" />,        
-        cName: 'nav-text',
-        cNav: 'current-nav',
+        cName: "nav-text",
+        cNav: "current-nav",
         role:["User"]
     },
     {
         title: "Blog status",
         path: "/blogstatus",
         icon: <IoIosPaper />,
-        cName: 'nav-text',
-        cNav: 'current-nav',
+        cName: "nav-text",
+        cNav: "current-nav",
         role:["Admin"]
     },     
     {
         title: "Admins",
         path: "/getalladmin",
         icon: <FaCrown />,
-        cName: 'nav-text',
-        cNav: 'current-nav',
+        cName: "nav-text",
+        cNav: "current-nav",
         role:["Admin"]
     },
     {
         title: "Users",
         path: "/getallusers",
         icon: <FaUser />,
-        cName: 'nav-text',
-        cNav: 'current-nav',
+        cName: "nav-text",
+        cNav: "current-nav",
         role:["Admin"]
     },
     {
     title: "Logout",
     path: "/logout" ,
     icon: <FaDoorOpen />,        
-    cName: 'nav-text',
-    cNav: 'current-nav',
+    cName: "nav-text",
+    cNav: "current-nav",
     role:["User","Admin"]
     }   
 ]
