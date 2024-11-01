499a0e2fd37ba3d18f67e8f107e7ebe3bc763a68
Kumar <esnthos@amazon.com>
2024-11-02 01:33:16 +0530

    third commit

--- client/src/components/sidebar/SideBar.jsx
+++ client/src/components/sidebar/SideBar.jsx
@@ -2,9 +2,11 @@ import React, {useState} from 'react'
 import { Link,useLocation } from 'react-router-dom'
 import {FaBars} from 'react-icons/fa'
 import { AiOutlineClose } from 'react-icons/ai'
-import { SideBarData } from './SidebarData.jsx'
 import { IconContext } from 'react-icons/lib'
 import "./SideBar.css"
+import { MDBIcon } from 'mdb-react-ui-kit';
+import {  IoIosPaper } from "react-icons/io";
+import { FaCrown,FaDove,FaEarthAsia,FaUser,FaDoorOpen,FaRegEnvelope,FaSearchengin } from "react-icons/fa6";
 
 
 const Navbar = () => {
@@ -17,6 +19,81 @@ const Navbar = () => {
     let role = sessionStorage.getItem('role')
     let {pathname} = useLocation()
     
+    const SideBarData = [  
+        {
+            title: "World",
+            path: "/blogfeeds",
+            icon: <FaEarthAsia />,        
+            cName: "nav-text",
+            cNav: "current-nav",
+            role:["User","Admin"]
+        },
+        {
+            title: "Post",
+            path: "/createblogs",
+            icon: <FaDove />,        
+            cName: "nav-text",
+            cNav: "current-nav",
+            role:["User"]
+        },
+        {
+            title: "My Post",
+            path: "/myblogs",
+            icon: <FaRegEnvelope />,        
+            cName: "nav-text",
+            cNav: "current-nav",
+            role:["User"]
+        },
+        {
+            title: "Search",
+            path: "/searchblogs",
+            icon: <FaSearchengin />,        
+            cName: "nav-text",
+            cNav: "current-nav",
+            role:["User"]
+        },
+        {
+            title: "Settings",
+            path: "/profilesettings",
+            icon: <MDBIcon fas icon="cog" />,        
+            cName: "nav-text",
+            cNav: "current-nav",
+            role:["User"]
+        },
+        {
+            title: "Blog status",
+            path: "/blogstatus",
+            icon: <IoIosPaper />,
+            cName: "nav-text",
+            cNav: "current-nav",
+            role:["Admin"]
+        },     
+        {
+            title: "Admins",
+            path: "/getalladmin",
+            icon: <FaCrown />,
+            cName: "nav-text",
+            cNav: "current-nav",
+            role:["Admin"]
+        },
+        {
+            title: "Users",
+            path: "/getallusers",
+            icon: <FaUser />,
+            cName: "nav-text",
+            cNav: "current-nav",
+            role:["Admin"]
+        },
+        {
+        title: "Logout",
+        path: "/logout" ,
+        icon: <FaDoorOpen />,        
+        cName: "nav-text",
+        cNav: "current-nav",
+        role:["User","Admin"]
+        }   
+    ]
+
      return (
          <>
              <IconContext.Provider value={{color: "#fff"}}>
